a414e0373514eaecf9081f248b8755c065ba0aab
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-25 21:03:37 -0700

    server.js file started

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 // import express module to our server
+const express = require('express');
 // import require("./db/db.json")
+const db = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
+const port = 3001;
 // create app variable pointing to new express object(express())
+const app = express();
 // app.use(*middleware*){
 // -json, urlencoded, staticify(public)}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // 
 // get/delete/post requests:
 // 
 // get request to send index.html, route: '/'
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 // get request to send notes.html, route: '/notes'
-// 
+//
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 // get request to fetch our api to send notes from db.json, route: '/api/notes'
+app.get('/api/notes',(req,res)=>
+  
+);
 // post request to fetch our api to modify with parsed req.body data and push post to db.json with fs.writeFile, route: '/api/notes', return res.json;
-// delete request to delete specific note, *EXTRA CRED* remove specific data from db.json and push with fs.writeFile,  route: '/api/notes/:notes_id', return res.json;
-// app.listen
+app.post('/api/notes', (req, res) => 
 
+);
+// delete request to delete specific note, *EXTRA CRED* remove specific data from db.json and push with fs.writeFile,  route: '/api/notes/:notes_id', return res.json;
+app.delete('/api/notes', (req, res) =>
+
+);
+// app.listen
+app.listen(port, () =>
+console.log(`App listening at http://localhost:${port}`));
 
 
 // GIVEN a note-taking application
