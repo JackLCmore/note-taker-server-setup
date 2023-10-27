9ffb92f37fc3a07ce93cc2e9133242d7fbe6c0f1
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-27 00:04:08 -0700

    finished README.md removed comments, updated based json contents

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# note-taker-server-setup
+ # Note Taker Server Setup
+
+ [Please Visit The Deployed Application](https://note-taker-server-setup-3efa15ce36dc.herokuapp.com)
+
+ ![img](./public/assets/images/Screenshot%202023-10-26%20235639.png)
+
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+
+  ## License
+    
+  MIT License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  Copyright 2023 Jack Lunchick-Seymour
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    
+  ## Description
+    
+  The Note Taker is a simple application that takes in a user note and stores it for later use on the heroku database. My task in this project was to complete all the back end routing having to do with the application's server.
+    
+  ## Installation
+    
+  There is no need to install the Note Taker, please visit the deployed website [here](https://note-taker-server-setup-3efa15ce36dc.herokuapp.com)
+    
+  ## Usage
+    
+  To use the Note Taker just visit the website and start logging your notes down! Come back at any time to view your previously saved notes!
+    
+  ## Tests
+    
+  All testing of this application was done within Node.js.
+    
+  ## Questions
+
+  For any questions please contact me at any of the following:
+    
+  Github: https://github.com/JackLCmore 
+  
+  Email: jack.lcmore@gmail.com 
+  
+  LinkedIn: https://www.linkedin.com/in/jack-seymour-b0b2b0292/
--- server.js
+++ server.js
@@ -1,43 +1,30 @@
-// import express module to our server
 const express = require('express');
-// import require("./db/db.json")
+
 const uuid = require('./helpers/uuid')
 const db = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 const port = process.env.PORT || 3001;
-// create app variable pointing to new express object(express())
+
 const app = express();
-// app.use(*middleware*){
-// -json, urlencoded, staticify(public)}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// 
-// get/delete/post requests:
-// 
-// get request to send index.html, route: '/'
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-// get request to send notes.html, route: '/notes'
-//
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-// get request to fetch our api to send notes from db.json, route: '/api/notes'
+
 app.get('/api/notes',(req, res)=>{
-// console.log(db)
-//   console.log(req);
-//   console.log(res);
-// return res.json;
 res.json(db);
 });
-// post request to fetch our api to modify with parsed req.body data and push post to db.json with fs.writeFile, route: '/api/notes', return res.json;
+
 app.post('/api/notes', (req, res) => {
-    // console.log(req.body)
-    // newDB.push(req.body);
-    // console.log(newDB);
     const { title, text, id } = req.body;
       
     const newNote = {
@@ -50,7 +37,6 @@ app.post('/api/notes', (req, res) => {
 
     fs.writeFile('./db/db.json', JSON.stringify(db) , (err) => {
     err ? console.error(err) : console.log('note logged')
-    // res.json(db);
     });
 
     res.json(db);
@@ -61,21 +47,5 @@ app.post('/api/notes', (req, res) => {
 //     // console.log(res);
 //     // return res.json;
 // });
-// app.listen
 app.listen(port, () =>
 console.log(`App listening at http://localhost:${port}`));
-
-
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
-// WHEN I click on the Save button
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
-// WHEN I click on the "New Note" button in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
